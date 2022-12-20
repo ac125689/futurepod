068c33d9760369cbed559bb59b904ce09e0075a4
ac125689 <81823797+ac125689@users.noreply.github.com>
2022-12-20 07:54:31 -0500

    Update Admin.py

--- Admin.py
+++ Admin.py
@@ -1,5 +1,8 @@
 import streamlit as st
+import datetime as dt
 
+h = dt.datetime.now().hour
+m = dt.datetime.now().minute
 
 
 
@@ -8,8 +11,13 @@ def admin():
     username = st.sidebar.text_input('Username')
     password = st.sidebar.text_input('Password',type='password')
     if st.sidebar.checkbox("Login"):
-        if password == 'ADMIN':
+        if password == f'ADMIN {h} : {m}':
             st.success(f'Logined as {username}')
+            tab1, tab2 = st.tabs(['Video or Podcast Uplode','comeing soon'])
+            with tab1:
+                st.subheader('Video or Podcast Uplode')
+            with tab2:
+                st.subheader('comeing soon')
 
-            
+
 
