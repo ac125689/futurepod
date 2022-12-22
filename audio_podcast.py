5021b45af3032bb4adea4315252b0fca1aed6128
ac125689 <81823797+ac125689@users.noreply.github.com>
2022-12-22 08:16:33 -0500

    s

--- audio_podcast.py
+++ audio_podcast.py
@@ -2,4 +2,4 @@ import streamlit as st
 from Admin import podcast_audio
 
 def audio():
-    st.header('Audio Podcast')
+    st.title('Audio Podcast')
--- video_podcast.py
+++ video_podcast.py
@@ -2,4 +2,4 @@ import streamlit as st
 from Admin import podcast_video
 
 def video():
-    st.header("Video Podcast")
+    st.title("Video Podcast")
